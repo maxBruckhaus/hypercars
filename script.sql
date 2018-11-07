9eda6fd952fa00d7711148ebd0173f67a4970534
marioOrtegaGarcia <mario.j.ortega.garcia@gmail.com>
2018-11-06 18:57:59 -0800

    Adding Insert Queries

--- script.sql
+++ script.sql
@@ -45,3 +45,27 @@ CREATE TABLE manufacturer (
     m_nation VARCHAR (10)    NOT NULL,
     m_sales  DECIMAL (10, 2) NOT NULL
 );
+INSERT INTO vehicle
+        (v_vin, v_license, v_year, v_make, v_model, v_color, v_price) VALUES
+        (1,2,3,4,5,6,7),
+        (1,2,3,4,5,6,7);
+INSERT INTO hypercars
+        (h_vin, h_engine, h_topSpeed, h_trans, h_weight) VALUES
+        (1,2,3,4,5),
+        (1,2,3,4,5);
+INSERT INTO engine
+        (e_model, e_make, e_cylinders, e_hp, e_torque, e_size) VALUES
+        (1,2,3,4,5,6),
+        (1,2,3,4,5,6);
+INSERT INTO transmission
+        (t_model, t_gears, t_type) VALUES
+        (1,2,3),
+        (1,2,3);
+INSERT INTO wheels
+        (w_model, e_leather, e_turbo, e_spoiler, e_tint, e_convertible) VALUES
+        (1,2,3,4,5,6),
+        (1,2,3,4,5,6);
+INSERT INTO manufacturer
+        (m_name, m_nation, m_sales) VALUES
+        (1,2,3),
+        (1,2,3);
