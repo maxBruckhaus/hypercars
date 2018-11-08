e19fdd9cb98cd997710f9a84bb7bd37ec11811af
marioOrtegaGarcia <mario.j.ortega.garcia@gmail.com>
2018-11-07 21:14:35 -0800

    progressing queries count 14

--- script.sql
+++ script.sql
@@ -13,7 +13,8 @@ CREATE TABLE IF NOT EXISTS hypercars (
     h_engine   VARCHAR (10) NOT NULL,
     h_topSpeed DECIMAL (3)  NOT NULL,
     h_trans    VARCHAR (10) NOT NULL,
-    h_weight   DECIMAL (3)  NOT NULL
+    h_weight   DECIMAL (3)  NOT NULL,
+    h_wheel    VARCHAR (10)   NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS engine (
@@ -88,66 +89,66 @@ INSERT INTO vehicle
 	("SCBCR63W55C024793", "123ABE7", 2005, "Bentley", "Continental GT", "RED", 83945);
 
 INSERT INTO hypercars
-        (h_vin, h_engine, h_topSpeed, h_trans, h_weight) VALUES
-	("JH4KA2650HC000268", "NVPVK6NE", 136, "BKRKZ", 1993),
-	("4JGBB86E27A199749", "QU875TE8", 201, "RY85H", 2489),
-	("3B7HF13Y81G193584", "R6NAYRET", 216, "9562C", 2075),
-	("1HGCT2B88DA000025", "F9V5B0H4", 156, "XZMOH", 2639),
-	("JH4DB8580RS000024", "F5UD9B4L", 272, "MO7OL", 1829),
-	("4S3BK6354S6355265", "3ESQ1YMY", 296, "DFWY5", 1821),
-	("5NPEB4AC1DH576656", "U5LSNFMZ", 207, "XE8YG", 1929),
-	("JH4KA4576KC031014", "XF8OIUQ2", 103, "A259G", 1695),
-	("JH4NA1260MT001906", "08A7XDMB", 107, "94CMC", 1682),
-	("1FAFP55U91A180689", "4G2FK0EI", 197, "J3WO8", 2772),
-	("SCA1S684X4UX07444", "U71AZ4GB", 194, "ARFAI", 2725),
-	("3VWRA69M74M033915", "7U6F42X5", 138, "CTRHW", 1600),
-	("1GNEK13T7YJ204464", "NKVDM332", 262, "ZZMB0", 2570),
-	("2C3CCAET4CH256062", "4S11N0XC", 168, "76H1W", 2288),
-	("ZFFXS41A7X0114158", "O62PTZ1N", 263, "5VDJB", 2152),
-	("ZFFEZ58E170153605", "QPBS0F1M", 187, "LVMS0", 1798),
-	("ZFFEW58A660144998", "Y3GP756Y", 200, "T786J", 2299),
-	("ZFFWL44A330131232", "GH01ME9J", 257, "SP0PJ", 2224),
-	("ZFFWP50A0V0106629", "KVQW1Z3K", 253, "659TT", 2402),
-	("ZFFEW58L170151456", "39L2Y02Z", 179, "54MXT", 1801),
-	("ZFFAB54A080014783", "DK4MQEZM", 180, "OB9UN", 1976),
-	("SCBCR63W55C024793", "PU69GAWZ", 132, "2W0WV", 1821),
-	("SCBFT7ZA3FC041545", "QAXYHVY6", 157, "58QOP", 2662),
-	("SCBZS42A5CCX05753", "A4BHKQL5", 183, "JEL1R", 1508),
-	("SCBLC37F04CX09874", "QXEZJCHG", 173, "80R8D", 1782),
-	("SCBBP9ZA0AC062683", "SXTCKS3V", 122, "GN4WS", 1574),
-	("SCBBB7ZH1EC118746", "LL7D6884", 147, "XW9HN", 2027),
-	("SCBCR63W55C024793", "2WGW2ZJH", 158, "AADF5", 1741);
+        (h_vin, h_engine, h_topSpeed, h_trans, h_weight, h_wheel) VALUES
+	("JH4KA2650HC000268", "NVPVK6NE", 136, "BKRKZ", 1993, "V6UKMC"),
+	("4JGBB86E27A199749", "QU875TE8", 201, "RY85H", 2489, "3T4774"),
+	("3B7HF13Y81G193584", "R6NAYRET", 216, "9562C", 2075, "ARBYW5"),
+	("1HGCT2B88DA000025", "F9V5B0H4", 156, "XZMOH", 2639, "F1ZY06"),
+	("JH4DB8580RS000024", "F5UD9B4L", 272, "MO7OL", 1829, "962V3D"),
+	("4S3BK6354S6355265", "3ESQ1YMY", 296, "DFWY5", 1821, "6CSNS3"),
+	("5NPEB4AC1DH576656", "U5LSNFMZ", 207, "XE8YG", 1929, "FLSGEK"),
+	("JH4KA4576KC031014", "XF8OIUQ2", 103, "A259G", 1695, "7Q7NH2"),
+	("JH4NA1260MT001906", "08A7XDMB", 107, "94CMC", 1682, "6E8TX7"),
+	("1FAFP55U91A180689", "4G2FK0EI", 197, "J3WO8", 2772, "86NNTT"),
+	("SCA1S684X4UX07444", "U71AZ4GB", 194, "ARFAI", 2725, "1E7P81"),
+	("3VWRA69M74M033915", "7U6F42X5", 138, "CTRHW", 1600, "1TD6GP"),
+	("1GNEK13T7YJ204464", "NKVDM332", 262, "ZZMB0", 2570, "XD62CM"),
+	("2C3CCAET4CH256062", "4S11N0XC", 168, "76H1W", 2288, "5EJXV8"),
+	("ZFFXS41A7X0114158", "O62PTZ1N", 263, "5VDJB", 2152, "VFMDJS"),
+	("ZFFEZ58E170153605", "QPBS0F1M", 187, "LVMS0", 1798, "C8BR7A"),
+	("ZFFEW58A660144998", "Y3GP756Y", 200, "T786J", 2299, "SS3CVF"),
+	("ZFFWL44A330131232", "GH01ME9J", 257, "SP0PJ", 2224, "T4PD0N"),
+	("ZFFWP50A0V0106629", "KVQW1Z3K", 253, "659TT", 2402, "JKQBAH"),
+	("ZFFEW58L170151456", "39L2Y02Z", 179, "54MXT", 1801, "G1YM7N"),
+	("ZFFAB54A080014783", "DK4MQEZM", 180, "OB9UN", 1976, "TTCEK7"),
+	("SCBCR63W55C024793", "PU69GAWZ", 132, "2W0WV", 1821, "SFJJ7H"),
+	("SCBFT7ZA3FC041545", "QAXYHVY6", 157, "58QOP", 2662, "RFIT0O"),
+	("SCBZS42A5CCX05753", "A4BHKQL5", 183, "JEL1R", 1508, "JZR2WL"),
+	("SCBLC37F04CX09874", "QXEZJCHG", 173, "80R8D", 1782, "D8HE5U"),
+	("SCBBP9ZA0AC062683", "SXTCKS3V", 122, "GN4WS", 1574, "R1VLJW"),
+	("SCBBB7ZH1EC118746", "LL7D6884", 147, "XW9HN", 2027, "1L6IGJ"),
+	("SCBCR63W55C024793", "2WGW2ZJH", 158, "AADF5", 1741, "KGER55");
 
 INSERT INTO engine
         (e_model, e_make, e_cylinders, e_hp, e_torque, e_size) VALUES
-	("NVPVK6NE", "Acura", "BKRKZ", 6, 602, 2.8),
-	("QU875TE8", "Mercedes Benz", "RY85H", 6, 532, 7.9),
-	("R6NAYRET", "Dodge", "9562C", 8, 764, 1.6),
-	("F9V5B0H4", "Honda", "XZMOH", 4, 709, 7.0),
-	("F5UD9B4L", "Acura", "MO7OL", 4, 774, 5.5),
-	("3ESQ1YMY", "Subaru", "DFWY5", 6, 764, 6.7),
-	("U5LSNFMZ", "Hyundai", "XE8YG", 6, 626, 3.1),
-	("XF8OIUQ2", "Acura", "A259G", 6, 676, 5.2),
-	("08A7XDMB", "Acura", "94CMC", 6, 588, 1.6),
-	("4G2FK0EI", "Ford", "J3WO8", 6, 671, 2.9),
-	("U71AZ4GB", "Rolls Royce", "ARFAI", 12, 606, 5.7),
-	("7U6F42X5", "Volkswagen", "CTRHW", 6, 677, 2.3),
-	("NKVDM332", "Chevrolet", "ZZMB0", 6, 630, 2.5),
-	("4S11N0XC", "Chrysler", "76H1W", 6, 689, 3.6),
-	("O62PTZ1N", "Ferrari", "5VDJB", 12, 675, 4.2),
-	("QPBS0F1M", "Ferrari", "LVMS0", 12, 691, 3.9),
-	("Y3GP756Y", "Ferrari", "T786J", 12, 771, 2.9),
-	("GH01ME9J", "Ferrari", "SP0PJ", 12, 577, 4.9),
-	("KVQW1Z3K", "Ferrari", "659TT", 12, 739, 3.5),
-	("39L2Y02Z", "Ferrari", "54MXT", 12, 627, 2.6),
-	("DK4MQEZM", "Ferrari", "OB9UN", 12, 567, 4.1),
-	("PU69GAWZ", "Bentley", "2W0WV", 12, 626, 1.2),
-	("QAXYHVY6", "Bentley", "58QOP", 12, 608, 4.1),
-	("A4BHKQL5", "Bentley", "JEL1R", 12, 581, 4.7),
-	("QXEZJCHG", "Bentley", "80R8D", 12, 807, 5.6),
-	("SXTCKS3V", "Bentley", "GN4WS", 12, 781, 5.3),
-	("LL7D6884", "Bentley", "XW9HN", 12, 568, 3.6),
-	("2WGW2ZJH", "Bentley", "AADF5", 12, 545, 2.7);
+	("NVPVK6NE", "Acura", 6, 496, 602, 2.8),
+	("QU875TE8", "Mercedes Benz", 6, 438, 532, 7.9),
+	("R6NAYRET", "Dodge", 8, 510, 764, 1.6),
+	("F9V5B0H4", "Honda", 4, 372, 709, 7.0),
+	("F5UD9B4L", "Acura", 4, 440, 774, 5.5),
+	("3ESQ1YMY", "Subaru", 6, 351, 764, 6.7),
+	("U5LSNFMZ", "Hyundai", 6, 497, 626, 3.1),
+	("XF8OIUQ2", "Acura", 6, 477, 676, 5.2),
+	("08A7XDMB", "Acura", 6, 540, 588, 1.6),
+	("4G2FK0EI", "Ford", 6, 429, 671, 2.9),
+	("U71AZ4GB", "Rolls Royce", 12, 594, 606, 5.7),
+	("7U6F42X5", "Volkswagen", 6, 448, 677, 2.3),
+	("NKVDM332", "Chevrolet", 6, 365, 630, 2.5),
+	("4S11N0XC", "Chrysler", 6, 521, 689, 3.6),
+	("O62PTZ1N", "Ferrari", 12, 428, 675, 4.2),
+	("QPBS0F1M", "Ferrari", 12, 394, 691, 3.9),
+	("Y3GP756Y", "Ferrari", 12, 414, 771, 2.9),
+	("GH01ME9J", "Ferrari", 12, 584, 577, 4.9),
+	("KVQW1Z3K", "Ferrari", 12, 587, 739, 3.5),
+	("39L2Y02Z", "Ferrari", 12, 514, 627, 2.6),
+	("DK4MQEZM", "Ferrari", 12, 555, 567, 4.1),
+	("PU69GAWZ", "Bentley", 12, 367, 626, 1.2),
+	("QAXYHVY6", "Bentley", 12, 378, 608, 4.1),
+	("A4BHKQL5", "Bentley", 12, 485, 581, 4.7),
+	("QXEZJCHG", "Bentley", 12, 500, 807, 5.6),
+	("SXTCKS3V", "Bentley", 12, 398, 781, 5.3),
+	("LL7D6884", "Bentley", 12, 579, 568, 3.6),
+	("2WGW2ZJH", "Bentley", 12, 311, 545, 2.7);
 
 INSERT INTO transmission
         (t_model, t_gears, t_type) VALUES
@@ -182,34 +183,34 @@ INSERT INTO transmission
 
 INSERT INTO wheels
         (w_model, w_make, w_height, w_width, w_rim, w_diameter) VALUES
-        ("BKRKZ", "CIARW", 24, 13.64, 8.06, 29.71),
-        ("RY85H", "JYFZP", 20, 12.30, 8.23, 22.25),
-        ("9562C", "QQXKH", 24, 14.29, 8.59, 26.41),
-        ("XZMOH", "TWZNB", 24, 13.76, 8.35, 18.27),
-        ("MO7OL", "OAZAX", 22, 16.85, 8.29, 27.85),
-        ("DFWY5", "HNVQZ", 25, 12.09, 8.89, 21.24),
-        ("XE8YG", "CIARW", 24, 16.96, 7.99, 27.00),
-        ("A259G", "JYFZP", 24, 14.18, 8.20, 26.04),
-        ("94CMC", "QQXKH", 21, 15.21, 7.58, 24.93),
-        ("J3WO8", "TWZNB", 20, 17.40, 7.91, 26.06),
-        ("ARFAI", "OAZAX", 24, 17.99, 8.49, 26.05),
-        ("CTRHW", "HNVQZ", 20, 12.39, 7.32, 20.38),
-        ("ZZMB0", "CIARW", 24, 16.62, 7.84, 21.62),
-        ("76H1W", "JYFZP", 23, 16.53, 7.03, 23.41),
-        ("5VDJB", "QQXKH", 25, 13.78, 8.88, 19.35),
-        ("LVMS0", "TWZNB", 20, 17.35, 7.26, 25.40),
-        ("T786J", "OAZAX", 20, 14.41, 7.24, 23.56),
-        ("SP0PJ", "HNVQZ", 25, 14.46, 7.18, 25.36),
-        ("659TT", "CIARW", 24, 12.91, 7.97, 18.15),
-        ("54MXT", "JYFZP", 22, 14.95, 7.21, 19.32),
-        ("OB9UN", "QQXKH", 21, 16.39, 7.46, 26.33),
-        ("2W0WV", "TWZNB", 22, 16.66, 7.01, 24.58),
-        ("58QOP", "OAZAX", 25, 13.35, 7.14, 21.17),
-        ("JEL1R", "HNVQZ", 21, 13.27, 8.40, 18.64),
-        ("80R8D", "CIARW", 21, 12.66, 8.55, 20.00),
-        ("GN4WS", "JYFZP", 24, 17.15, 7.60, 29.35),
-        ("XW9HN", "QQXKH", 22, 14.15, 7.15, 26.40),
-        ("AADF5", "TWZNB", 20, 17.38, 8.67, 22.75);
+        ("V6UKMC", "CIARW", 24, 13.64, 8.06, 29.71),
+        ("3T4774", "JYFZP", 20, 12.30, 8.23, 22.25),
+        ("ARBYW5", "QQXKH", 24, 14.29, 8.59, 26.41),
+        ("F1ZY06", "TWZNB", 24, 13.76, 8.35, 18.27),
+        ("962V3D", "OAZAX", 22, 16.85, 8.29, 27.85),
+        ("6CSNS3", "HNVQZ", 25, 12.09, 8.89, 21.24),
+        ("FLSGEK", "CIARW", 24, 16.96, 7.99, 27.00),
+        ("7Q7NH2", "JYFZP", 24, 14.18, 8.20, 26.04),
+        ("6E8TX7", "QQXKH", 21, 15.21, 7.58, 24.93),
+        ("86NNTT", "TWZNB", 20, 17.40, 7.91, 26.06),
+        ("1E7P81", "OAZAX", 24, 17.99, 8.49, 26.05),
+        ("1TD6GP", "HNVQZ", 20, 12.39, 7.32, 20.38),
+        ("XD62CM", "CIARW", 24, 16.62, 7.84, 21.62),
+        ("5EJXV8", "JYFZP", 23, 16.53, 7.03, 23.41),
+        ("VFMDJS", "QQXKH", 25, 13.78, 8.88, 19.35),
+        ("C8BR7A", "TWZNB", 20, 17.35, 7.26, 25.40),
+        ("SS3CVF", "OAZAX", 20, 14.41, 7.24, 23.56),
+        ("T4PD0N", "HNVQZ", 25, 14.46, 7.18, 25.36),
+        ("JKQBAH", "CIARW", 24, 12.91, 7.97, 18.15),
+        ("G1YM7N", "JYFZP", 22, 14.95, 7.21, 19.32),
+        ("TTCEK7", "QQXKH", 21, 16.39, 7.46, 26.33),
+        ("SFJJ7H", "TWZNB", 22, 16.66, 7.01, 24.58),
+        ("RFIT0O", "OAZAX", 25, 13.35, 7.14, 21.17),
+        ("JZR2WL", "HNVQZ", 21, 13.27, 8.40, 18.64),
+        ("D8HE5U", "CIARW", 21, 12.66, 8.55, 20.00),
+        ("R1VLJW", "JYFZP", 24, 17.15, 7.60, 29.35),
+        ("1L6IGJ", "QQXKH", 22, 14.15, 7.15, 26.40),
+        ("KGER55", "TWZNB", 20, 17.38, 8.67, 22.75);
 
 INSERT INTO manufacturer
         (m_name, m_nation, m_sales) VALUES
@@ -258,34 +259,166 @@ INSERT INTO extras
 	("SCBBB7ZH1EC118746", 0, 1, 0, 0, 0, 0),
 	("SCBCR63W55C024793", 0, 1, 1, 0, 0, 1);
 
---"List all makers with vehicles costing moore than 100000"
-SELECT DISTINCT(m_name) FROM vehicle, manufacturer
-WHERE v_make = m_name AND
-        v_price > 100000
+	--List all makers with vehicles costing moore than 100000
+SELECT DISTINCT (m_name)
+  FROM vehicle,
+       manufacturer
+ WHERE v_make = m_name AND
+       v_price > 100000;
+
+ --List the vin number of all american cars that are convertible and have a torque between 600 and 700 with a top speed higher than 200
+ SELECT v_vin
+   FROM vehicle,
+	extras,
+	hypercars,
+	manufacturer,
+	engine
+  WHERE v_vin = ex_vin AND
+	v_vin = h_vin AND
+	v_make = m_name AND
+	h_engine = e_model AND
+	m_nation LIKE "UNITED STATES" AND
+	h_topSpeed > 200 AND
+	ex_convertible = 1 AND
+	e_torque BETWEEN 600 AND 700;
 
 
---List all the convertible cars that have at least
-SELECT v_vin
-FROM vehicle, manufacturer, extras
-WHERE v_make = m_name AND
-	a_ = 1 AND
-	v_vin = e_vin;
 
---q2
+	--List the amount of manufacturers a country has
+	SELECT m_nation, COUNT(DISTINCT(m_name))
+	FROM manufacturer
+	GROUP BY m_nation
 
---q2
 
---q2
+	--Give me  the make model and year of lightest car
+SELECT v_year,
+       v_make,
+       v_model
+  FROM hypercars,
+       vehicle
+ WHERE v_price IN (
+           SELECT MIN(v_price) AS v_price
+             FROM vehicle
+       )
+ GROUP BY v_vin;
 
---q2
+--List countries who sell cars with a topspeed over 250
+SELECT m_nation
+FROM manufacturer, vehicle, hypercars
+WHERE v_vin = h_vin AND
+      h_topSpeed > 250 and
+      m_name = v_make;
 
---q2
+--Cost of all registered automatic cars from germany which have a turbo
+ SELECT SUM(v_price)
+   FROM vehicle,
+        manufacturer,
+        extras,
+        transmission,
+        hypercars
+  WHERE v_make = m_name AND
+        v_vin = ex_vin AND
+        v_vin = h_vin AND
+        h_trans = t_model AND
+        m_nation LIKE "GERMANY" AND
+        ex_turbo = 1 AND
+        t_type = 1;
 
---q2
 
---q2
+--Find how many manufacturers produce cars with heated seats and spoilers
+SELECT COUNT(DISTINCT(m_name))
+FROM manufacturer, vehicle, extras
+WHERE v_vin = ex_vin AND
+      m_name = v_make AND
+      ex_heated = 1 AND
+      ex_spoiler = 1;
 
---q2
+
+--Find the year where the most expensive car was produced in USA
+SELECT v_year
+FROM manufacturer, vehicle, hypercars
+WHERE v_vin = h_vin AND
+      m_nation = "UNITED STATES" AND
+      v_price = (SELECT MAX(v_price)
+                 FROM vehicle);
+
+
+--Find the manufacturer with the highest number of sales before the year 2000
+SELECT m_name
+FROM manufacturer, vehicle
+WHERE v_year < 2000 AND
+      m_sales = (SELECT MAX(m_sales)
+                 FROM manufacturer);
+
+
+--Find the nation that produces the slowest car in the year 2004 that also has a spoiler
+SELECT m_nation
+FROM manufacturer, extras, vehicle, hypercars
+WHERE ex_vin = v_vin AND
+      v_vin = h_vin AND
+      ex_spoiler = 1 AND
+      v_year = 2004 AND
+      h_topSpeed = (SELECT MIN(h_topSpeed)
+                    FROM hypercars);
+
+
+    --List the hypercars that have rim diameter lower than 7.68, sorted  by rim size  descending
+    SELECT v_year, v_make, v_model,
+           w_model,
+           w_rim
+      FROM vehicle,
+           hypercars,
+           wheels
+     WHERE v_vin = h_vin AND
+           h_wheel = w_model AND
+           w_rim < 7.68
+     ORDER BY w_rim DESC;
+
+
+
+--List hypercars with 6 gears and no turbo are made by Mercedes Benz
+SELECT h_vin
+FROM hypercars, manufacturer, extras, transmission, vehicle
+WHERE t_gears = 6 AND
+      ex_turbo = 0 AND
+      m_name = "Mercedes Benz" AND
+      v_make = m_name AND
+      h_vin = v_vin AND
+      ex_vin = v_vin;
+
+--List all the cars with the most popular colors
+SELECT v_year,
+       v_make,
+       v_model,
+       v_color
+FROM vehicle
+WHERE v_color IN (
+           SELECT v_color
+             FROM (
+                      SELECT v_color,
+                             count( * ) AS sum
+                        FROM vehicle
+                       GROUP BY v_color
+                  )
+                  AS colorCount
+            WHERE sum IN (
+                      SELECT MAX(sum)
+                        FROM (
+                                 SELECT v_color,
+                                        count( * ) AS sum
+                                   FROM vehicle
+                                  GROUP BY v_color
+                             )
+                  )
+       )
+ GROUP BY v_color,
+          v_vin;
+
+--Update a car's color
+UPDATE vehicle
+   SET v_color = 'RED'
+ WHERE v_vin = '1GNEK13T7YJ204464';
+
 
 --q2
 
